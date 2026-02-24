361b83489760d7f4a1423c14c3f5d5fc1923e378
mattma <linghan.ma@gmail.com>
2026-02-25 00:08:15 +0800

    add

---
+++ src/main/java/com/malinghan/marpc/marpc/MarpcApplication.java
@@ -0,0 +1,13 @@
+package com.malinghan.marpc.marpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malinghan/marpc/marpc/MarpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malinghan.marpc.marpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
